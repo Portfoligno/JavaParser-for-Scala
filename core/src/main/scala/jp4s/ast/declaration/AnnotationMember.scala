0717a86e3895e06f990ff25e278e9fa93a48cbb7
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-03 23:39:36 +0900

    Add `AnnotationMember`

---
+++ core/src/main/scala/jp4s/ast/declaration/AnnotationMember.scala
@@ -0,0 +1,35 @@
+package jp4s.ast
+package declaration
+
+import java.util.Optional
+
+import com.github.javaparser.ast.`type`.Type
+import com.github.javaparser.ast.expr.Expression
+import jp4s.ast.name.Simple
+import jp4s.utility.JavaList
+
+object AnnotationMember {
+  def apply(
+    modifiers: JavaList[Modifier],
+    annotations: JavaList[expression.Annotation],
+    `type`: Type,
+    name: Simple,
+    defaultValue: Optional[Expression]
+  ): AnnotationMember =
+    new AnnotationMember(
+      nodes(modifiers),
+      nodes(annotations),
+      `type`,
+      name,
+      defaultValue.orElse(null)
+    )
+
+  def unapply(m: AnnotationMember): Option[(
+    NodeList[Modifier],
+    NodeList[expression.Annotation],
+    Type,
+    Simple,
+    Optional[Expression])
+  ] =
+    Some((m.getModifiers, m.getAnnotations, m.getType, m.getName, m.getDefaultValue))
+}
--- core/src/main/scala/jp4s/ast/declaration/package.scala
+++ core/src/main/scala/jp4s/ast/declaration/package.scala
@@ -1,9 +1,10 @@
 package jp4s.ast
 
-import com.github.javaparser.ast.body.{AnnotationDeclaration, BodyDeclaration}
+import com.github.javaparser.ast.body.{AnnotationDeclaration, AnnotationMemberDeclaration, BodyDeclaration}
 
 package object declaration {
   type Annotation = AnnotationDeclaration
+  type AnnotationMember = AnnotationMemberDeclaration
 
   type Body = BodyDeclaration[_ <: BodyDeclaration[_]]
 }
