895e40316b8ae3b986429ea95322bd9903963cf8
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-11 07:27:30 +0900

    Represent the scope of `MethodReference` as the disjunction of `Type` and `Expression`

--- core/src/main/scala/jp4s/ast/expression/MethodReference.scala
+++ core/src/main/scala/jp4s/ast/expression/MethodReference.scala
@@ -1,30 +1,34 @@
 package jp4s.ast
 package expression
 
-import com.github.javaparser.ast.`type`.Type
+import com.github.javaparser.ast.expr.TypeExpr
+import jp4s.ast.`type`.Type
 import nejc4s.base.{JavaList, Optional}
 
 object MethodReference {
   import jp4s.syntax.optional._
 
   def apply(
-    scope: Expression,
+    scope: Either[Type, Expression],
     typeArguments: Optional[JavaList[Type]],
     identifier: Identifier
   ): MethodReference =
     new MethodReference(
-      scope,
+      scope.fold(new TypeExpr(_), identity),
       typeArguments.transform(nodeList).orElseNull,
       identifier
     )
 
   def unapply(r: MethodReference): Some[(
-    Expression,
+    Either[Type, Expression],
     Optional[JavaList[Type]],
     Identifier
   )] =
     Some((
-      r.getScope,
+      r.getScope match {
+        case t: TypeExpr => Left(t.getType)
+        case x: _ => Right(x)
+      },
       r.getTypeArguments.covary,
       Identifier.unsafeFromString(r.getIdentifier)
     ))
