4159946a8c4c39630a61404ec920b09ca7c5e4d9
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-11 15:43:08 +0900

    Add `Continue`

---
+++ core/src/main/scala/jp4s/ast/statement/Continue.scala
@@ -0,0 +1,16 @@
+package jp4s.ast
+package statement
+
+import nejc4s.base.Optional
+
+object Continue {
+  import jp4s.syntax.optional._
+
+  def apply(label: Optional[Identifier]): Continue =
+    new Continue(
+      label.transform(simpleNameNode).orElseNull
+    )
+
+  def unapply(c: Continue): Some[Optional[Identifier]] =
+    Some(c.getLabel.map(identifier))
+}
--- core/src/main/scala/jp4s/ast/statement/package.scala
+++ core/src/main/scala/jp4s/ast/statement/package.scala
@@ -1,12 +1,13 @@
 package jp4s.ast
 
-import com.github.javaparser.ast.stmt.{AssertStmt, BlockStmt, BreakStmt}
+import com.github.javaparser.ast.stmt.{AssertStmt, BlockStmt, BreakStmt, ContinueStmt}
 
 package object statement {
   type Assert = AssertStmt
   type Block = BlockStmt
   type Break = BreakStmt
   type CatchClause = com.github.javaparser.ast.stmt.CatchClause
+  type Continue = ContinueStmt
 
   type Statement = com.github.javaparser.ast.stmt.Statement
 }
