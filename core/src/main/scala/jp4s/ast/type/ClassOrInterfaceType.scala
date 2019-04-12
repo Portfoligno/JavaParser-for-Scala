34cb1d4909ca07c7f03d10c6f78c43092fdf1e4c
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-12 11:21:50 +0900

    Add `ClassOrInterfaceType`

---
+++ core/src/main/scala/jp4s/ast/type/ClassOrInterfaceType.scala
@@ -0,0 +1,35 @@
+package jp4s.ast
+package `type`
+
+import jp4s.ast.expression.Annotation
+import nejc4s.base.{JavaList, Optional}
+
+object ClassOrInterfaceType {
+  import jp4s.syntax.optional._
+
+  def apply(
+    scope: Optional[ClassOrInterfaceType],
+    name: Identifier,
+    typeArguments: Optional[JavaList[Type]],
+    annotations: JavaList[Annotation]
+  ): ClassOrInterfaceType =
+    new ClassOrInterfaceType(
+      scope.orElseNull,
+      simpleNameNode(name),
+      typeArguments.transform(nodeList).orElseNull,
+      nodeList(annotations)
+    )
+
+  def unapply(t: ClassOrInterfaceType): Some[(
+    Optional[ClassOrInterfaceType],
+    Identifier,
+    Optional[JavaList[Type]],
+    JavaList[Annotation]
+  )] =
+    Some((
+      t.getScope,
+      identifier(t.getName),
+      t.getTypeArguments.covary,
+      t.getAnnotations
+    ))
+}
