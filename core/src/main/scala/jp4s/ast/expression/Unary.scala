907a9beb208e06b67c36ae8bf0b25b77daf3f579
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-14 18:44:42 +0900

    Implement `Unary.Operator`

--- core/src/main/scala/jp4s/ast/expression/Unary.scala
+++ core/src/main/scala/jp4s/ast/expression/Unary.scala
@@ -3,25 +3,63 @@ package jp4s.ast.expression
 import com.github.javaparser.ast.expr.UnaryExpr
 import com.github.javaparser.ast.expr.UnaryExpr.Operator._
 
+import scala.collection.mutable
+
 object Unary {
-  type Operator = UnaryExpr.Operator
+  type OperatorEnum = UnaryExpr.Operator
 
-  object Plus extends Factory(PLUS)
-  object Minus extends Factory(MINUS)
-  object PrefixIncrement extends Factory(PREFIX_INCREMENT)
-  object PrefixDecrement extends Factory(PREFIX_DECREMENT)
-  object LogicalComplement extends Factory(LOGICAL_COMPLEMENT)
-  object BitwiseComplement extends Factory(BITWISE_COMPLEMENT)
-  object PostfixIncrement extends Factory(POSTFIX_INCREMENT)
-  object PostfixDecrement extends Factory(POSTFIX_DECREMENT)
+  case object Plus extends Operator(PLUS)
+  case object Minus extends Operator(MINUS)
+  case object PrefixIncrement extends Operator(PREFIX_INCREMENT)
+  case object PrefixDecrement extends Operator(PREFIX_DECREMENT)
+  case object LogicalComplement extends Operator(LOGICAL_COMPLEMENT)
+  case object BitwiseComplement extends Operator(BITWISE_COMPLEMENT)
+  case object PostfixIncrement extends Operator(POSTFIX_INCREMENT)
+  case object PostfixDecrement extends Operator(POSTFIX_DECREMENT)
 
 
-  sealed abstract class Factory(val operator: Operator) {
+  def apply(operator: Operator, expression: Expression): Unary =
+    operator(expression)
+
+  def unapply(u: Unary): Some[(
+    Operator,
+      Expression
+    )] =
+    Some((
+      Operator(u.getOperator),
+      u.getExpression
+    ))
+
+
+  private
+  val lookup = mutable.Map[OperatorEnum, Operator]()
+
+  object Operator {
+    // Ensure all cases are initialized
+    Plus
+    Minus
+    PrefixIncrement
+    PrefixDecrement
+    LogicalComplement
+    BitwiseComplement
+    PostfixIncrement
+    PostfixDecrement
+
+    def apply(enum: OperatorEnum): Operator =
+      lookup(enum)
+
+    def unapply(o: Operator): Some[OperatorEnum] =
+      Some(o.enum)
+  }
+
+  sealed abstract class Operator(private val enum: OperatorEnum) {
+    lookup += enum -> this
+
     def apply(expression: Expression): Unary =
-      new Unary(expression, operator)
+      new Unary(expression, enum)
 
     def unapply(u: Unary): Option[Expression] =
-      if (u.getOperator == operator) {
+      if (u.getOperator == enum) {
         Some(u.getExpression)
       } else {
         None
