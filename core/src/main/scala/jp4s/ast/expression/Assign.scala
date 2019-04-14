8c4d4e4fc31766f6f190369405a3ffef6b2f7d74
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-14 17:18:58 +0900

    Implement `Assign.Operator`

--- core/src/main/scala/jp4s/ast/expression/Assign.scala
+++ core/src/main/scala/jp4s/ast/expression/Assign.scala
@@ -3,29 +3,73 @@ package jp4s.ast.expression
 import com.github.javaparser.ast.expr.AssignExpr
 import com.github.javaparser.ast.expr.AssignExpr.Operator._
 
+import scala.collection.mutable
+
 object Assign {
-  type Operator = AssignExpr.Operator
+  type OperatorEnum = AssignExpr.Operator
 
-  object Plain extends Factory(ASSIGN)
-  object Plus extends Factory(PLUS)
-  object Minus extends Factory(MINUS)
-  object Multiply extends Factory(MULTIPLY)
-  object Divide extends Factory(DIVIDE)
-  object BinaryAnd extends Factory(BINARY_AND)
-  object BinaryOr extends Factory(BINARY_OR)
-  object Xor extends Factory(XOR)
-  object Remainder extends Factory(REMAINDER)
-  object LeftShift extends Factory(LEFT_SHIFT)
-  object SignedRightShift extends Factory(SIGNED_RIGHT_SHIFT)
-  object UnsignedRightShift extends Factory(UNSIGNED_RIGHT_SHIFT)
+  case object Plain extends Operator(ASSIGN)
+  case object Plus extends Operator(PLUS)
+  case object Minus extends Operator(MINUS)
+  case object Multiply extends Operator(MULTIPLY)
+  case object Divide extends Operator(DIVIDE)
+  case object BinaryAnd extends Operator(BINARY_AND)
+  case object BinaryOr extends Operator(BINARY_OR)
+  case object Xor extends Operator(XOR)
+  case object Remainder extends Operator(REMAINDER)
+  case object LeftShift extends Operator(LEFT_SHIFT)
+  case object SignedRightShift extends Operator(SIGNED_RIGHT_SHIFT)
+  case object UnsignedRightShift extends Operator(UNSIGNED_RIGHT_SHIFT)
 
 
-  sealed abstract class Factory(val operator: Operator) {
+  def apply(operator: Operator, target: Expression, value: Expression): Assign =
+    operator(target, value)
+
+  def unapply(a: Assign): Some[(
+    Operator,
+    Expression,
+    Expression
+  )] =
+    Some((
+      Operator(a.getOperator),
+      a.getTarget,
+      a.getValue
+    ))
+
+
+  private
+  val lookup = mutable.Map[OperatorEnum, Operator]()
+
+  object Operator {
+    // Ensure all cases are initialized
+    Plain
+    Plus
+    Minus
+    Multiply
+    Divide
+    BinaryAnd
+    BinaryOr
+    Xor
+    Remainder
+    LeftShift
+    SignedRightShift
+    UnsignedRightShift
+
+    def apply(enum: OperatorEnum): Operator =
+      lookup(enum)
+
+    def unapply(o: Operator): Some[OperatorEnum] =
+      Some(o.enum)
+  }
+
+  sealed abstract class Operator(private val enum: OperatorEnum) {
+    lookup += enum -> this
+
     def apply(target: Expression, value: Expression): Assign =
-      new Assign(target, value, operator)
+      new Assign(target, value, enum)
 
     def unapply(a: Assign): Option[(Expression, Expression)] =
-      if (a.getOperator == operator) {
+      if (a.getOperator == enum) {
         Some((a.getTarget, a.getValue))
       } else {
         None
