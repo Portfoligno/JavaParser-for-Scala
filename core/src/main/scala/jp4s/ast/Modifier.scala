92b9882ebb97a9c501864e8c08bee25555386d61
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-15 00:39:25 +0900

    Implement `Modifier.Keyword`

--- core/src/main/scala/jp4s/ast/Modifier.scala
+++ core/src/main/scala/jp4s/ast/Modifier.scala
@@ -2,30 +2,66 @@ package jp4s.ast
 
 import com.github.javaparser.ast.Modifier.Keyword._
 
+import scala.collection.mutable
+
 object Modifier {
-  type Keyword = com.github.javaparser.ast.Modifier.Keyword
+  type KeywordEnum = com.github.javaparser.ast.Modifier.Keyword
 
-  object Default extends Factory(DEFAULT)
-  object Public extends Factory(PUBLIC)
-  object Protected extends Factory(PROTECTED)
-  object Private extends Factory(PRIVATE)
-  object Abstract extends Factory(ABSTRACT)
-  object Static extends Factory(STATIC)
-  object Final extends Factory(FINAL)
-  object Transient extends Factory(TRANSIENT)
-  object Volatile extends Factory(VOLATILE)
-  object Synchronized extends Factory(SYNCHRONIZED)
-  object Native extends Factory(NATIVE)
-  object Strictfp extends Factory(STRICTFP)
-  object Transitive extends Factory(TRANSITIVE)
-  object PackagePrivate extends Factory(PACKAGE_PRIVATE)
+  case object Default extends Keyword(DEFAULT)
+  case object Public extends Keyword(PUBLIC)
+  case object Protected extends Keyword(PROTECTED)
+  case object Private extends Keyword(PRIVATE)
+  case object Abstract extends Keyword(ABSTRACT)
+  case object Static extends Keyword(STATIC)
+  case object Final extends Keyword(FINAL)
+  case object Transient extends Keyword(TRANSIENT)
+  case object Volatile extends Keyword(VOLATILE)
+  case object Synchronized extends Keyword(SYNCHRONIZED)
+  case object Native extends Keyword(NATIVE)
+  case object Strictfp extends Keyword(STRICTFP)
+  case object Transitive extends Keyword(TRANSITIVE)
+  case object PackagePrivate extends Keyword(PACKAGE_PRIVATE)
 
 
-  sealed abstract class Factory(val keyword: Keyword) {
+  def apply(keyword: Keyword): Modifier =
+    keyword()
+
+  def unapply(m: Modifier): Some[Keyword] =
+    Some(Keyword(m.getKeyword))
+
+
+  private
+  val lookup = mutable.Map[KeywordEnum, Keyword]()
+
+  object Keyword {
+    // Ensure all cases are initialized
+    Default
+    Public
+    Protected
+    Private
+    Abstract
+    Static
+    Final
+    Transient
+    Volatile
+    Synchronized
+    Native
+    Strictfp
+    Transitive
+    PackagePrivate
+
+    def apply(enum: KeywordEnum): Keyword =
+      lookup(enum)
+
+    def unapply(k: Keyword): Some[KeywordEnum] =
+      Some(k.enum)
+  }
+
+  sealed abstract class Keyword(private val enum: KeywordEnum) {
     def apply(): Modifier =
-      new Modifier(keyword)
+      new Modifier(enum)
 
     def unapply(modifier: Modifier): Boolean =
-      modifier.getKeyword == keyword
+      modifier.getKeyword == enum
   }
 }
