20e8b1296fcf0c8e09c950de3f830ea659d41702
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-14 16:05:13 +0900

    Implement `Parameter.Variance`

--- core/src/main/scala/jp4s/ast/declaration/Parameter.scala
+++ core/src/main/scala/jp4s/ast/declaration/Parameter.scala
@@ -6,7 +6,38 @@ import jp4s.ast.expression.Annotation
 import nejc4s.base.JavaList
 
 object Parameter {
-  object Plain {
+  case object Plain extends Variance(false)
+  case object VarArgs extends Variance(true)
+
+  def apply(
+    variance: Parameter.Variance,
+    modifiers: JavaList[Modifier],
+    annotations: JavaList[Annotation],
+    `type`: Type,
+    varArgsAnnotations: JavaList[Annotation],
+    name: Identifier
+  ): Parameter =
+    variance(modifiers, annotations, `type`, varArgsAnnotations, name)
+
+  def unapply(p: Parameter): Option[(
+    Parameter.Variance,
+    JavaList[Modifier],
+    JavaList[Annotation],
+    Type,
+    JavaList[Annotation],
+    Identifier
+  )] =
+    Some((
+      if (p.isVarArgs) VarArgs else Plain,
+      p.getModifiers,
+      p.getAnnotations,
+      p.getType,
+      p.getVarArgsAnnotations,
+      identifier(p.getName)
+    ))
+
+
+  sealed abstract class Variance(private val isVarArgs: Boolean) {
     def apply(
       modifiers: JavaList[Modifier],
       annotations: JavaList[Annotation],
@@ -18,7 +49,7 @@ object Parameter {
         nodeList(modifiers),
         nodeList(annotations),
         `type`,
-        false,
+        isVarArgs,
         nodeList(varArgsAnnotations),
         simpleNameNode(name)
       )
@@ -30,7 +61,9 @@ object Parameter {
       JavaList[Annotation],
       Identifier
     )] =
-      if (!p.isVarArgs) {
+      if (p.isVarArgs ^ isVarArgs) {
+        None
+      } else {
         Some((
           p.getModifiers,
           p.getAnnotations,
@@ -38,46 +71,6 @@ object Parameter {
           p.getVarArgsAnnotations,
           identifier(p.getName)
         ))
-      } else {
-        None
-      }
-  }
-
-
-  object VarArgs {
-    def apply(
-      modifiers: JavaList[Modifier],
-      annotations: JavaList[Annotation],
-      `type`: Type,
-      varArgsAnnotations: JavaList[Annotation],
-      name: Identifier
-    ): Parameter =
-      new Parameter(
-        nodeList(modifiers),
-        nodeList(annotations),
-        `type`,
-        true,
-        nodeList(varArgsAnnotations),
-        simpleNameNode(name)
-      )
-
-    def unapply(p: Parameter): Option[(
-      JavaList[Modifier],
-      JavaList[Annotation],
-      Type,
-      JavaList[Annotation],
-      Identifier
-    )] =
-      if (p.isVarArgs) {
-        Some((
-          p.getModifiers,
-          p.getAnnotations,
-          p.getType,
-          p.getVarArgsAnnotations,
-          identifier(p.getName)
-        ))
-      } else {
-        None
       }
   }
 }
