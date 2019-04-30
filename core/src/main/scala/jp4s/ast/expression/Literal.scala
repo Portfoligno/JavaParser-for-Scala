c33758db7fa3e1d651984932c4eba86420a0822f
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-30 20:15:40 +0900

    Remove partial extractors of `Literal` and `LiteralByString`

--- core/src/main/scala/jp4s/ast/expression/Literal.scala
+++
@@ -1,6 +0,0 @@
-package jp4s.ast.expression
-
-object Literal {
-  def unapply(l: Literal): Boolean =
-    true
-}
--- core/src/main/scala/jp4s/ast/expression/LiteralByString.scala
+++
@@ -1,6 +0,0 @@
-package jp4s.ast.expression
-
-object LiteralByString {
-  def unapply(l: LiteralByString): Some[String] =
-    Some(l.getValue)
-}
