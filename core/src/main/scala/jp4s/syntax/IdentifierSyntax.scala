1024b21c2912c16572437ee3229f46989f76f805
Nicholas Yip <nicholasyip@obceit.cc>
2019-05-01 01:08:08 +0900

    Implement `IdInterpolator`

--- core/src/main/scala/jp4s/syntax/IdentifierSyntax.scala
+++ core/src/main/scala/jp4s/syntax/IdentifierSyntax.scala
@@ -11,22 +11,34 @@ trait IdentifierSyntax {
 }
 
 class IdStringContext(private val sc: StringContext) extends AnyVal {
-  def id(unused: Nothing*): Identifier =
-    macro IdMarco.expand
+  def id: IdInterpolator =
+    macro IdMarco.check
 }
 
 private
 object IdMarco {
-  def expand(c: whitebox.Context)(unused: c.Expr[Nothing]*): c.Expr[Identifier] = {
+  def check(c: whitebox.Context): c.Expr[IdInterpolator] = {
     import c.universe._
 
     c.prefix.tree match {
       case Apply(_, List(Apply(_, List(literal @ Literal(Constant(s: String)))))) =>
         if (Identifier.isIdentifier(s)) {
-          c.Expr(q"""$literal.asInstanceOf[_root_.jp4s.ast.Identifier]""")
+          c.Expr(q"""new _root_.jp4s.syntax.IdInterpolator($literal.asInstanceOf[_root_.jp4s.ast.Identifier])""")
         } else {
           c.abort(c.enclosingPosition, s"$s is not an identifier")
         }
     }
   }
+}
+
+class IdInterpolator(val value: Identifier) extends AnyVal {
+  def apply(unused: Nothing*): Identifier =
+    value
+
+  def unapplySeq(s: String): Option[Seq[Nothing]] =
+    if (s == value) {
+      Some(Seq.empty)
+    } else {
+      None
+    }
 }
