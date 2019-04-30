d5f51170b993e5b472322ca57e656f3d3d62dd09
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-30 18:50:18 +0900

    Add `Boxed`

---
+++ core/src/main/scala/jp4s/extra/resolution/Boxed.scala
@@ -0,0 +1,19 @@
+package jp4s.extra.resolution
+
+import com.github.javaparser.ast.`type`.PrimitiveType.Primitive
+import com.github.javaparser.resolution.types.{ResolvedPrimitiveType, ResolvedReferenceType}
+import jp4s.ast.`type`.PrimitiveType
+
+object Boxed {
+  private
+  val variances =
+    ResolvedPrimitiveType
+      .values()
+      .map(t =>
+        t.getBoxTypeQName -> PrimitiveType.Variance(Primitive.valueOf(t.name()))
+      )
+      .toMap
+
+  def unapply(t: ResolvedReferenceType): Option[PrimitiveType.Variance] =
+    variances.get(t.getQualifiedName)
+}
