d3a72cd1ac87aecc8563f04c3fb1ab6ceefe4601
Nicholas Yip <nicholasyip@obceit.cc>
2019-05-01 00:41:39 +0900

    Add `AllSyntax`

---
+++ core/src/main/scala/jp4s/syntax/AllSyntax.scala
@@ -0,0 +1,3 @@
+package jp4s.syntax
+
+trait AllSyntax extends IdentifierSyntax with OptionalSyntax
--- core/src/main/scala/jp4s/syntax/package.scala
+++ core/src/main/scala/jp4s/syntax/package.scala
@@ -1,6 +1,7 @@
 package jp4s
 
 package object syntax {
+  object all extends AllSyntax
   object identifier extends IdentifierSyntax
   object variable extends VariableSyntax
 
