6e75fb320e2be989584b7627a731370d3a5ce6d8
Nicholas Yip <nicholasyip@obceit.cc>
2019-05-01 22:41:56 +0900

    Implement `IdsInterpolator`

--- core/src/main/scala/jp4s/syntax/IdentifierSyntax.scala
+++ core/src/main/scala/jp4s/syntax/IdentifierSyntax.scala
@@ -1,6 +1,8 @@
 package jp4s.syntax
 
 import jp4s.ast.Identifier
+import jp4s.ast.Identifier.isIdentifier
+import nejc4s.alias.Nejl
 
 import scala.language.experimental.macros
 import scala.reflect.macros.whitebox
@@ -13,6 +15,9 @@ trait IdentifierSyntax {
 class IdStringContext(private val sc: StringContext) extends AnyVal {
   def id: IdInterpolator =
     macro IdMarco.check
+
+  def ids: IdsInterpolator =
+    macro IdMarco.checkMultiple
 }
 
 private
@@ -20,12 +25,49 @@ object IdMarco {
   def check(c: whitebox.Context): c.Expr[IdInterpolator] = {
     import c.universe._
 
+    def IdInterpolator: TypeSymbol = symbolOf[IdInterpolator]
+    def Identifier: TypeSymbol = symbolOf[Identifier]
+
     c.prefix.tree match {
       case Apply(_, List(Apply(_, List(literal @ Literal(Constant(s: String)))))) =>
-        if (Identifier.isIdentifier(s)) {
-          c.Expr(q"""new _root_.jp4s.syntax.IdInterpolator($literal.asInstanceOf[_root_.jp4s.ast.Identifier])""")
+        if (isIdentifier(s)) {
+          c.Expr(q"""new $IdInterpolator($literal.asInstanceOf[$Identifier])""")
         } else {
           c.abort(c.enclosingPosition, s"$s is not an identifier")
+        }
+    }
+  }
+
+  def checkMultiple(c: whitebox.Context): c.Expr[IdsInterpolator] = {
+    import c.universe._
+
+    def IdsInterpolator: TypeSymbol = symbolOf[IdsInterpolator]
+    def Identifier: TypeSymbol = symbolOf[Identifier]
+
+    c.prefix.tree match {
+      case Apply(_, List(Apply(_, List(Literal(Constant(s: String)))))) =>
+        val parts = s.split('.')
+        val invalid = parts.filterNot(isIdentifier)
+
+        if (invalid.isEmpty) {
+          val constants = parts.map(Constant(_)).view
+
+          c.Expr(q"""
+            new $IdsInterpolator(
+              _root_.nejc4s.alias.Nejl(..$constants).asInstanceOf[_root_.nejc4s.alias.Nejl[$Identifier]]
+            )
+          """)
+        } else {
+          c.abort(
+            c.enclosingPosition,
+            invalid match {
+              case Array(a) =>
+                s"$a is not an identifier"
+
+              case _ =>
+                s"${invalid.init.mkString("", ", ", " and ")}${invalid.last} are not identifiers"
+            }
+          )
         }
     }
   }
@@ -37,6 +79,18 @@ class IdInterpolator(val value: Identifier) extends AnyVal {
 
   def unapplySeq(s: String): Option[Seq[Nothing]] =
     if (s == value) {
+      Some(Seq.empty)
+    } else {
+      None
+    }
+}
+
+class IdsInterpolator(val value: Nejl[Identifier]) extends AnyVal {
+  def apply(unused: Nothing*): Nejl[Identifier] =
+    value
+
+  def unapplySeq(xs: Nejl[Identifier]): Option[Seq[Nothing]] =
+    if (xs == value) {
       Some(Seq.empty)
     } else {
       None
